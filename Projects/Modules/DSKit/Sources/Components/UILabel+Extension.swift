155fa4f23e87d33330a3ba45fd9e4ea6c93a7da3
HyeonJun Kang <khjj9865@naver.com>
2023-10-20 20:58:32 +0900

    feat: Font 설정 extension 추가

---
+++ Projects/Modules/DSKit/Sources/Components/UILabel+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UILabel+Extension.swift
+//  DSKitTests
+//
+//  Created by 강현준 on 2023/10/20.
+//  Copyright © 2023 TeamOne. All rights reserved.
+//
+
+import UIKit
+
+public extension UILabel {
+    @discardableResult
+    func setLabel(text: String, typo: SansNeo) -> Self {
+        self.text = text
+        self.font = .setFont(font: typo)
+
+        return self
+    }
+}
