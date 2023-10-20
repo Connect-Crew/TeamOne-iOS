1836e76a1c34c30d9055fa578bfb51fd58b804ac
HyeonJun Kang <khjj9865@naver.com>
2023-10-20 16:09:51 +0900

    feat: AppDelegate+DIContainer

---
+++ Projects/TeamOne/Sources/App/AppDelegate+DIExtension.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate+DIExtension.swift
+//  TeamOne
+//
+//  Created by 강현준 on 2023/10/20.
+//  Copyright © 2023 TeamOne. All rights reserved.
+//
+
+import Core
+import Domain
+
+extension AppDelegate {
+    var container: DIContainer {
+        DIContainer.shared
+    }
+
+    func registerDependencies() {
+
+        // MARK: - Repository
+
+
+        // MARK: - UseCase
+
+        
+        // MARK: - ViewModel
+
+
+    }
+}
