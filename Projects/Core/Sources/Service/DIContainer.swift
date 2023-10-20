d8883302b7b65a7c321807917e77d62073f67b48
HyeonJun Kang <khjj9865@naver.com>
2023-10-20 16:13:34 +0900

    feat: DIContainer resolve, register 추가.

--- Projects/Core/Sources/Service/DIContainer.swift
+++ Projects/Core/Sources/Service/DIContainer.swift
@@ -12,10 +12,19 @@ import Swinject
 public final class DIContainer {
     
     public static let shared = DIContainer()
+
     private let container: Container
     
     private init() {
         self.container = Container()
+    }
+
+    public func register<T>(interface: T.Type, implement: @escaping ((Resolver) -> T)) {
+        container.register(interface, factory: implement)
+    }
+
+    public func resolve<T>(_ serviceType: T.Type) -> T {
+        return container.resolve(serviceType)!
     }
 }
 
