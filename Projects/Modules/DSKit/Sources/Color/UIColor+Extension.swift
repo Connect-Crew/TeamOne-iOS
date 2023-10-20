4e57e550e08db4d6397563b9e16c903dc61fb1f3
HyeonJun Kang <khjj9865@naver.com>
2023-10-20 22:11:01 +0900

    feat: Color추가

--- Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
+++ Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
@@ -18,4 +18,14 @@ public extension UIColor {
 
 public struct TeamOneColor {
     public let backgroundDefault = UIColor(r: 253, g: 253, b: 253, a: 1)
+    public let mainBlue = UIColor(r: 0, g: 174, b: 228, a: 1)
+    public let mainlightColor = UIColor(r: 241, g: 252, b: 255, a: 1)
+    public let background = UIColor(r: 249, g: 250, b: 252, a: 1)
+    public let point = UIColor(r: 214, g: 37, b: 70, a: 1)
+    public let pointTwo = UIColor(r: 247, g: 205, b: 213, a: 1)
+    public let grayscaleEight = UIColor(r: 66, g: 66, b: 66, a: 1)
+    public let grayscaleSeven = UIColor(r: 97, g: 97, b: 97, a: 1)
+    public let grayscaleFive = UIColor(r: 158, g: 158, b: 158, a: 1)
+    public let grayscaleTwo = UIColor(r: 238, g: 238, b: 238, a: 1)
+    public let grayscaleOne = UIColor(r: 245, g: 245, b: 245, a: 1)
 }
