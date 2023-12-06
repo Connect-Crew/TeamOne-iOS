4210af874ae6256715b771f46e73f691e64cd6bc
이준영 <junlight94@gmail.com>
2023-12-06 22:27:03 +0900

    feat. Search 헤더뷰 작업

---
+++ Projects/TeamOne/Sources/App/Search/View/SearchHeaderView.swift
@@ -0,0 +1,68 @@
+//
+//  SearchHeaderView.swift
+//  TeamOne
+//
+//  Created by Junyoung on 12/6/23.
+//  Copyright © 2023 TeamOne. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+import Core
+import DSKit
+
+final class SearchHeaderView: UIView {
+    
+    private let backButton = UIButton().then {
+        $0.setImage(.image(dsimage: .backButtonImage), for: .normal)
+    }
+    
+    private let searchTextField = SearchTextField()
+    
+    private let searchButton = UIButton().then {
+        $0.setImage(.image(dsimage: .search), for: .normal)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        
+        // MARK: Back Button
+        addSubview(backButton)
+        backButton.snp.makeConstraints { make in
+            make.height.width.equalTo(24)
+            make.left.equalToSuperview().inset(24)
+            make.top.bottom.equalToSuperview().inset(9)
+        }
+        
+        // MARK: TextField
+        addSubview(searchTextField)
+        searchTextField.snp.makeConstraints { make in
+            make.left.equalTo(backButton.snp.right).offset(6.5)
+            make.top.bottom.equalToSuperview().inset(7)
+        }
+        
+        // MARK: Search Button
+        addSubview(searchButton)
+        searchButton.snp.makeConstraints { make in
+            make.height.width.equalTo(24)
+            make.left.equalTo(searchTextField.snp.right).offset(6.5)
+            make.right.equalToSuperview().inset(24)
+            make.top.bottom.equalToSuperview().inset(9)
+        }
+        
+        self.setBaseShadow(radius: 8)
+    }
+}
--- Projects/TeamOne/Sources/App/Search/View/SearchMainView.swift
+++ Projects/TeamOne/Sources/App/Search/View/SearchMainView.swift
@@ -6,16 +6,40 @@
 //  Copyright © 2023 TeamOne. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
 import UIKit
+import SnapKit
+import Then
+import Core
+import DSKit
 
-class SearchMainView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class SearchMainView: UIView {
+    
+    let searchHeader = SearchHeaderView()
+    let contentView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        addSubview(searchHeader)
+        searchHeader.snp.makeConstraints { make in
+            make.top.left.right.equalTo(safeAreaLayoutGuide)
+            make.height.equalTo(42)
+        }
+        
+        addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(searchHeader.snp.bottom)
+            make.left.right.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
 }
