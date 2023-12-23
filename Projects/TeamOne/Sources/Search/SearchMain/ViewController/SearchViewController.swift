0f761842fb70ee019a9003bb4762dbfefdbe1a31
이준영 <junlight94@gmail.com>
2023-12-23 19:51:02 +0900

    feat. viewcontroller deinit 출력 문구, DiffableDataSoure 적용

--- Projects/TeamOne/Sources/Search/SearchMain/ViewController/SearchViewController.swift
+++ Projects/TeamOne/Sources/Search/SearchMain/ViewController/SearchViewController.swift
@@ -14,6 +14,16 @@ import Then
 
 final class SearchViewController: ViewController {
     
+    private enum Section {
+        case main
+    }
+    
+    private var searchItems = [String]()
+    
+    typealias Item = String
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, Item>!
+    
     private let viewModel: SearchViewModel
     
     private let mainView = SearchMainView()
@@ -53,8 +63,34 @@ final class SearchViewController: ViewController {
         mainView.searchTableView.register(SearchHistoryCell.self,
                                     forCellReuseIdentifier: SearchHistoryCell.defaultReuseIdentifier)
         
+        dataSource = UITableViewDiffableDataSource<Section, Item>(tableView: self.mainView.searchTableView) { [weak self]
+            (tableView, indexPath, item) -> SearchHistoryCell? in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchHistoryCell.defaultReuseIdentifier,
+                                                           for: indexPath) as? SearchHistoryCell else { return nil }
+            guard let this = self else { return nil }
+            cell.bind(title: item)
+            
+            cell.deleteHistoryButton.rx.tap
+                .withUnretained(this)
+                .subscribe(onNext: { this, _ in
+                    this.deleteHistory.onNext(cell.historyTitle)
+                })
+                .disposed(by: cell.disposeBag)
+            
+            return cell
+        }
+        
+        applySnapshot()
+        
         mainView.searchResultTableView.register(HomeTableViewCell.self,
                                                 forCellReuseIdentifier: HomeTableViewCell.defaultReuseIdentifier)
+    }
+    
+    func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(searchItems, toSection: .main)
+        self.dataSource.apply(snapshot)
     }
     
     override func bind() {
@@ -72,21 +108,28 @@ final class SearchViewController: ViewController {
         let output = viewModel.transform(input: input)
         
         output.searchHistoryList
-            .bind(to: mainView.searchTableView.rx.items(
-                cellIdentifier: SearchHistoryCell.defaultReuseIdentifier,
-                cellType: SearchHistoryCell.self)) { [weak self] (_, element, cell) in
-                    guard let self = self else { return }
-                    
-                    cell.bind(title: element)
-                    
-                    cell.deleteHistoryButton.rx.tap
-                        .withUnretained(self)
-                        .subscribe(onNext: { this, _ in
-                            this.deleteHistory.onNext(cell.historyTitle)
-                        })
-                        .disposed(by: cell.disposeBag)
-            }
+            .withUnretained(self)
+            .subscribe(onNext: { this, result in
+                this.searchItems = result
+                this.applySnapshot()
+            })
             .disposed(by: disposeBag)
+        
+//            .bind(to: mainView.searchTableView.rx.items(
+//                cellIdentifier: SearchHistoryCell.defaultReuseIdentifier,
+//                cellType: SearchHistoryCell.self)) { [weak self] (_, element, cell) in
+//                    guard let self = self else { return }
+//                    
+//                    cell.bind(title: element)
+//                    
+//                    cell.deleteHistoryButton.rx.tap
+//                        .withUnretained(self)
+//                        .subscribe(onNext: { this, _ in
+//                            this.deleteHistory.onNext(cell.historyTitle)
+//                        })
+//                        .disposed(by: cell.disposeBag)
+//            }
+//            .disposed(by: disposeBag)
         
         output.searchIsEmpty
             .observe(on: MainScheduler.instance)
@@ -108,17 +151,11 @@ final class SearchViewController: ViewController {
             .bind(to: mainView.searchResultTableView.rx.items(
                 cellIdentifier: HomeTableViewCell.defaultReuseIdentifier,
                 cellType: HomeTableViewCell.self)) { [weak self] (_, element, cell) in
-                    guard let self = self else { return }
-                    
                     cell.selectionStyle = .none
                     cell.prepareForReuse()
                     cell.backgroundColor = .teamOne.background
                     cell.initSetting(project: element)
                 }
             .disposed(by: disposeBag)
-    }
-    
-    deinit {
-        print("search deinit")
     }
 }
