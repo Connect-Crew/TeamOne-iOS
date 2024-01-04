5372f1a0f4006a864b2065ccbed2cb89b6f02e96
HyeonJun Kang <khjj9865@naver.com>
2024-01-04 11:41:36 +0900

    feat: DSKit VC+Extension ErrorAlert 추가

--- Projects/Modules/DSKit/Sources/Extension/UIViewController+ErrorAlert.swift
+++ Projects/Modules/DSKit/Sources/Extension/UIViewController+ErrorAlert.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import Core
+import SnapKit
+import Then
+import TeamOneNetwork
+
+extension UIViewController {
+    
+    func showErrorAlert(
+        error: Error,
+        finishSubject: PublishSubject<Void>
+    ) {
+        
+        var alert = ResultAlertView_Image_Title_Content_Alert(
+            image: .warnning,
+            title: "에러",
+            content: "",
+            availableCancle: false
+        )
+        
+        if let error = error as? APIError {
+            
+            switch error {
+                
+            case .network(let statusCode, let message):
+                alert.content = "\(statusCode): \(message)"
+                self.presentResultAlertView_Image_Title_Content(
+                    source: self,
+                    alert: alert
+                )
+                
+            case .notToken:
+                break
+                
+            case .unknown:
+                alert.content = "알 수 없는 에러\n고객센터로 문의해주세요"
+                
+            }
+        } else {
+            alert.content = "알 수 없는 에러\n고객센터로 문의해주세요"
+        }
+    }
+    
+}
