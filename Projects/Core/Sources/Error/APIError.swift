b3e634aff7a599a6897e0ae3f38e761a4af5b296
HyeonJun Kang <khjj9865@naver.com>
2024-01-06 22:15:18 +0900

    fix: ErrorEntity, APIError 위치 수정

---
+++ Projects/Core/Sources/Error/APIError.swift
@@ -0,0 +1,9 @@
+//
+//  APIError.swift
+//  Core
+//
+//  Created by 강현준 on 1/6/24.
+//  Copyright © 2024 TeamOne. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Core/Sources/Error/ErrorEntity.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorEntity.swift
+//  Core
+//
+//  Created by 강현준 on 1/6/24.
+//  Copyright © 2024 TeamOne. All rights reserved.
+//
+
+import Foundation
