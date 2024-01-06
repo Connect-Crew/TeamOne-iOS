894e5c8278d264ee945b38b52fbb24012570d026
HyeonJun Kang <khjj9865@naver.com>
2024-01-06 15:14:56 +0900

    fix: tuist plugins path

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,8 +2,8 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToRoot("/Tuist/Plugins/DependencyPlugin")),
-        .local(path: .relativeToRoot("/Tuist/Plugins/ConfigPlugin")),
-        .local(path: .relativeToRoot("/Tuist/Plugins/EnvPlugin")),
+        .local(path: .relativeToRoot("Tuist/Plugins/DependencyPlugin")),
+        .local(path: .relativeToRoot("Tuist/Plugins/ConfigPlugin")),
+        .local(path: .relativeToRoot("Tuist/Plugins/EnvPlugin")),
     ]
 )
