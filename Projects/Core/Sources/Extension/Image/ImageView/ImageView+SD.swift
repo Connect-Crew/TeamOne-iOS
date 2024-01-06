6baf285a216667002a78fda3299da56f17af9186
HyeonJun Kang <khjj9865@naver.com>
2024-01-06 16:59:07 +0900

    feat: Image 관련 기능 추가

---
+++ Projects/Core/Sources/Extension/Image/ImageView/ImageView+SD.swift
@@ -0,0 +1,53 @@
+//
+//  ImageView+SD.swift
+//  Core
+//
+//  Created by 강현준 on 1/6/24.
+//  Copyright © 2024 TeamOne. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+public extension UIImageView {
+    
+    static var baseNetworkURL: String {
+        return "http://teamone.kro.kr:9080"
+    }
+    
+    /// path: api통신 후 가져온 이미지 주소
+    func setTeamOneImage(path: String?) {
+        
+        if let urlString = path,
+           let url = URL(string: Self.baseNetworkURL + urlString) {
+            self.sd_setImage(with: url)
+        }
+    }
+    
+    static func pathToImage(path: [String?], completion: @escaping ([UIImage?]) -> Void) {
+        var images: [UIImage?] = Array(repeating: nil, count: path.count)
+            let group = DispatchGroup()
+            
+            for (index, pathString) in path.enumerated() {
+                guard let pathToString = pathString,
+                    let url = URL(string: baseNetworkURL + pathToString) else {
+                    continue
+                }
+                
+                group.enter() // 다운로드 시작
+                
+                SDWebImageManager.shared.loadImage(with: url, options: [], progress: nil) { (image, _, error, _, _, _) in
+                    if let downloadedImage = image {
+                        images[index] = downloadedImage // 이미지 다운로드 성공
+                    } else {
+                        print("Error downloading image: \(error?.localizedDescription ?? "Unknown error")")
+                    }
+                    group.leave() // 다운로드 완료(성공/실패)를 그룹에 알립니다.
+                }
+            }
+            
+            group.notify(queue: .main) {
+                completion(images) // 모든 이미지 다운로드가 완료되면 콜백을 호출합니다.
+            }
+    }
+}
