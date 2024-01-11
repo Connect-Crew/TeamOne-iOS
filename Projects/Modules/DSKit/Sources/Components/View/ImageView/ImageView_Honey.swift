217a833b40b314f858c4f50da319279e9a6fbc51
HyeonJun Kang <khjj9865@naver.com>
2024-01-11 16:50:50 +0900

    feat: 꿀단지 이미지뷰 컴포넌트화

--- Projects/Modules/DSKit/Sources/Components/View/ImageView/ImageView_Honey.swift
+++ Projects/Modules/DSKit/Sources/Components/View/ImageView/ImageView_Honey.swift
@@ -6,4 +6,124 @@
 //  Copyright © 2024 TeamOne. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public final class ImageView_Honey: UIImageView {
+    
+    public enum HoneyType {
+        case small
+        case large
+        
+        static func setLevel(temparature: Double?) -> Int {
+            return 1
+        }
+    }
+    
+    public enum HoneySmallType {
+        case one
+        case two
+        case three
+        case four
+        
+        var honeyImage: UIImage? {
+            switch self {
+            case .one:
+                return .image(dsimage: .honeySmall1)
+            case .two:
+                return .image(dsimage: .honeySmall2)
+            case .three:
+                return .image(dsimage: .honeySmall3)
+            case .four:
+                return .image(dsimage: .honeySmall4)
+            }
+        }
+        
+        init(temparature: Double?) {
+            let level = HoneyType.setLevel(temparature: temparature)
+            
+            switch level {
+            case 1:
+                self = .one
+            case 2:
+                self = .two
+            case 3:
+                self = .three
+            case 4:
+                self = .four
+            default:
+                fatalError()
+            }
+        }
+    }
+    
+    public enum HoneyLargeType {
+        case one
+        case two
+        case three
+        case four
+        
+        var honeyImage: UIImage? {
+            switch self {
+            case .one:
+                return .image(dsimage: .honeyLarge1)
+            case .two:
+                return .image(dsimage: .honeyLarge2)
+            case .three:
+                return .image(dsimage: .honeyLarge3)
+            case .four:
+                return .image(dsimage: .honeyLarge4)
+            }
+        }
+        
+        init(temparature: Double?) {
+            let level = HoneyType.setLevel(temparature: temparature)
+            
+            switch level {
+            case 1:
+                self = .one
+            case 2:
+                self = .two
+            case 3:
+                self = .three
+            case 4:
+                self = .four
+            default:
+                fatalError()
+            }
+        }
+    }
+    
+    private let type: HoneyType
+    private var temparature: Double?
+    
+    public init(type: HoneyType) {
+        self.type = type
+        super.init(frame: .zero)
+    }
+    
+    public func setHoney(temparature: Double?) {
+        
+        self.temparature = temparature
+        
+        switch type {
+        case .small:
+            setSmallImage()
+        case .large:
+            setLargeImage()
+        }
+    }
+    
+    func setSmallImage() {
+        let type = HoneySmallType.init(temparature: temparature)
+        self.image = type.honeyImage
+    }
+    
+    func setLargeImage() {
+        let type = HoneyLargeType.init(temparature: temparature)
+        self.image = type.honeyImage
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
