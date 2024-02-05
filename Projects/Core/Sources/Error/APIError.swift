97ce233ed58ae473ed62d9a37a247978379cc68b
HyeonJun Kang <khjj9865@naver.com>
2024-02-05 14:14:04 +0900

    feat: 네트워크 에러 추가

--- Projects/Core/Sources/Error/APIError.swift
+++ Projects/Core/Sources/Error/APIError.swift
@@ -12,6 +12,7 @@ public enum APIError: Error, Equatable , LocalizedError{
     case network(statusCode: Int, message: String)
     case notToken
     case decodingError
+    case notConnectedToInternet
     case unknown
     
     public var errorDescription: String? {
@@ -22,12 +23,19 @@ public enum APIError: Error, Equatable , LocalizedError{
             return "유저 토큰이 없습니다. 로그아웃됩니다."
         case .decodingError:
             return "디코딩 에러 발생"
+        case .notConnectedToInternet: 
+            return "네트워크 연결 문제가 발생했습니다."
         case .unknown:
             return "알 수 없는 에러"
         }
     }
 
     public init(error: ErrorEntity) {
-        self = .network(statusCode: error.status, message: error.message)
+        
+        if error.status == 401 {
+            self = .notToken
+        } else {
+            self = .network(statusCode: error.status, message: error.message)
+        }
     }
 }
