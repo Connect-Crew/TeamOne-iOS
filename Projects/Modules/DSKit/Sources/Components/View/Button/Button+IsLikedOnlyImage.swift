4f7500b1e3b42b67b14b43199654ddd3262534c0
HyeonJun Kang <khjj9865@naver.com>
2024-02-13 12:54:18 +0900

    feat: 관심 프로젝트 셀 추가

--- Projects/Modules/DSKit/Sources/Components/View/Button/Button+IsLikedOnlyImage.swift
+++ Projects/Modules/DSKit/Sources/Components/View/Button/Button+IsLikedOnlyImage.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2024 TeamOne. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class Button_IsLikedOnlyImage: UIButton {
+    
+    static let likedImage: UIImage? = .image(dsimage: .heartsolid)
+    static let unLikedImage: UIImage? = .image(dsimage: .heartline)
+    
+    public var isLiked: Bool {
+        didSet {
+            if isLiked {
+                self.setImage(Self.likedImage, for: .normal)
+            } else {
+                self.setImage(Self.unLikedImage, for: .normal)
+            }
+        }
+    }
+    
+    public init(isLiked: Bool) {
+        self.isLiked = isLiked
+        super.init(frame: .zero)
+        
+        let image = isLiked ? Self.likedImage : Self.unLikedImage
+        self.setImage(image, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
