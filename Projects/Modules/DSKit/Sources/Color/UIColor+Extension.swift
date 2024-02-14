e14b3bba5c53a2f00e2cd69e7ed028f981fe9777
HyeonJun Kang <khjj9865@naver.com>
2024-02-14 14:03:15 +0900

    feat: 색상 타입속성으로 추가

--- Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
+++ Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
@@ -14,6 +14,21 @@ public extension UIColor {
     }
 
     static let teamOne = TeamOneColor()
+    
+    static let backgroundDefault = UIColor(r: 253, g: 253, b: 253, a: 1)
+    static let mainColor = UIColor(r: 0, g: 174, b: 228, a: 1)
+    static let mainlightColor = UIColor(r: 241, g: 252, b: 255, a: 1)
+    static let background = UIColor(r: 249, g: 250, b: 252, a: 1)
+    static let point = UIColor(r: 214, g: 37, b: 70, a: 1)
+    static let pointTwo = UIColor(r: 247, g: 205, b: 213, a: 1)
+    static let grayscaleEight = UIColor(r: 66, g: 66, b: 66, a: 1)
+    static let grayscaleSeven = UIColor(r: 97, g: 97, b: 97, a: 1)
+    static let grayscaleFive = UIColor(r: 158, g: 158, b: 158, a: 1)
+    static let grayscaleTwo = UIColor(r: 238, g: 238, b: 238, a: 1)
+    static let grayscaleOne = UIColor(r: 245, g: 245, b: 245, a: 1)
+    static let white = UIColor(r: 253, g: 253, b: 253, a: 1)
+    static let bgColor = UIColor(r: 249, g: 250, b: 252, a: 1)
+    static let transparent = UIColor(r: 0, g: 0, b: 0, a: 0.7)
 }
 
 public struct TeamOneColor {
