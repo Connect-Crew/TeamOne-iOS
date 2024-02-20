9204113188bdb607ee8eecaa402ff018633b6760
HyeonJun Kang <khjj9865@naver.com>
2024-02-20 20:20:57 +0900

    refactor: 프로젝트에서 사용되는 색상을 UIColor의 static으로 추가

--- Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
+++ Projects/Modules/DSKit/Sources/Color/UIColor+Extension.swift
@@ -26,9 +26,15 @@ public extension UIColor {
     static let grayscaleFive = UIColor(r: 158, g: 158, b: 158, a: 1)
     static let grayscaleTwo = UIColor(r: 238, g: 238, b: 238, a: 1)
     static let grayscaleOne = UIColor(r: 245, g: 245, b: 245, a: 1)
-    static let white = UIColor(r: 253, g: 253, b: 253, a: 1)
     static let bgColor = UIColor(r: 249, g: 250, b: 252, a: 1)
     static let transparent = UIColor(r: 0, g: 0, b: 0, a: 0.7)
+    static let levelOneColor = UIColor(r: 255, g: 251, b: 214, a: 1)
+    static let levelTwoColor = UIColor(r: 255, g: 251, b: 138, a: 1)
+    static let levelThreeColor = UIColor(r: 254, g: 234, b: 53, a: 1)
+    static let levelFourColor = UIColor(r: 255, g: 206, b: 16, a: 1)
+    static let levelFiveColor = UIColor(r: 252, g: 171, b: 13, a: 1)
+    static let levelSixColor = UIColor(r: 246, g: 107, b: 15, a: 1)
+    static let levelSevenColor = UIColor(r: 195, g: 38, b: 27, a: 1)
 }
 
 public struct TeamOneColor {
